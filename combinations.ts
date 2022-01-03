8c0c2c3b5209d1cb78a591caa9f61e7713889dd3
rldnrl <devpark1002@gmail.com>
2022-01-03 16:26:34 +0900

    feat: combination

---
+++ combinations.ts
@@ -0,0 +1,16 @@
+const getCombinations = <T>(array: T[], selectNumber: number) => {
+  const results: T[][] = [];
+
+  if (selectNumber === 1) {
+    return array.map((element) => [element]);
+  }
+
+  array.forEach((fixed, index, origin) => {
+    const rest = origin.slice(index + 1);
+    const combinations = getCombinations(rest, selectNumber - 1);
+    const attached = combinations.map((combination) => [fixed, ...combination]);
+    results.push(...attached);
+  });
+
+  return results;
+};
