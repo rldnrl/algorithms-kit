c7305a475ce997fd1b379600a7406473faf30712
Kiwook, Park <devpark1002@gmail.com>
2022-01-18 20:55:30 +0900

    feat: set up jest

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+// jest.config.js
+module.exports = {
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+  ],
+  transform: {
+    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+    "^.+\\.(js|jsx|ts|tsx)$": [
+      "babel-jest",
+      {
+        presets: [
+          ["@babel/preset-env", { targets: { node: "current" } }],
+          "@babel/preset-typescript",
+        ],
+      },
+    ],
+  },
+  transformIgnorePatterns: ["/node_modules/"],
+};
