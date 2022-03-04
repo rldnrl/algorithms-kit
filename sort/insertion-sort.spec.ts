e3fcd9e29d43cea8d5e85c5383d8c00466cc8fcf
rldnrl <devpark1002@gmail.com>
2022-03-04 13:41:04 +0900

    삽입 정렬 테스트 코드

---
+++ sort/insertion-sort.spec.ts
@@ -0,0 +1,8 @@
+import insertionSort from './insertion-sort'
+
+describe("Insertion Sort", () => {
+  it("Success Sort", () => {
+    expect(insertionSort([4, 5, 1, 2, 3, 0])).toStrictEqual([0, 1, 2, 3, 4, 5])
+    expect(insertionSort([10, 3, 2, 1, 100, 50])).toStrictEqual([1, 2, 3, 10, 50, 100])
+  })
+})
