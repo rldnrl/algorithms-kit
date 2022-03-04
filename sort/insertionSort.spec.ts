27ebdd7327b36ba96f9464be37a45ad8ca9434c0
rldnrl <devpark1002@gmail.com>
2022-03-04 13:44:11 +0900

    camelCase로 변경

--- sort/insertionSort.spec.ts
+++ sort/insertionSort.spec.ts
@@ -1,8 +1,7 @@
-import selectionSort from "./selection-sort";
-import insertionSort from "./insertion-sort";
+import insertionSort from './insertionSort'
 
-describe("Selection Sort", () => {
-  it("success sort", () => {
+describe("Insertion Sort", () => {
+  it("Success Sort", () => {
     expect(insertionSort([4, 5, 1, 2, 3, 0])).toStrictEqual([0, 1, 2, 3, 4, 5])
     expect(insertionSort([10, 3, 2, 1, 100, 50])).toStrictEqual([1, 2, 3, 10, 50, 100])
   })
--- sort/selectionSort.spec.ts
+++ sort/selectionSort.spec.ts
@@ -1,7 +1,8 @@
-import insertionSort from './insertion-sort'
+import selectionSort from "./selectionSort";
+import insertionSort from "./insertionSort";
 
-describe("Insertion Sort", () => {
-  it("Success Sort", () => {
+describe("Selection Sort", () => {
+  it("success sort", () => {
     expect(insertionSort([4, 5, 1, 2, 3, 0])).toStrictEqual([0, 1, 2, 3, 4, 5])
     expect(insertionSort([10, 3, 2, 1, 100, 50])).toStrictEqual([1, 2, 3, 10, 50, 100])
   })
